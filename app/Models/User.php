55726d0273a8cc45e798aae9e9ac753e7f43e25e
Adnan Wahab <adnandhw@MacBook-Adnandhw.local>
2026-01-17 19:49:33 +0700

    update users  dan paksa masukin build

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,16 @@ class User extends Authenticatable implements FilamentUser
     // protected $connection = 'mysql';
     protected $fillable = ['name','email','password'];
 
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
     public function canAccessPanel(Panel $panel): bool
     {
         return true;
